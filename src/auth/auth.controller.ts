40bdf4bcf5582e7a25eca877c214f31f8f108f4f
jarrodschilling <jarrodschilling@gmail.com>
2024-03-31 10:46:12 -0600

    login/reg done

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,19 @@
+import { Controller, Post, Body } from '@nestjs/common';
+import { CreateUserDto } from 'src/user/dto/user.dto';
+import { UserService } from 'src/user/user.service';
+import { LoginDto } from './dto/auth.dto';
+
+@Controller('auth')
+export class AuthController {
+    constructor(private userService: UserService) {}
+
+    @Post('register')
+    async registerUser(@Body() dto:CreateUserDto) {
+        return await this.userService.create(dto)
+    }
+
+    @Post('login')
+    async login(@Body() dto:LoginDto) {
+        
+    }
+}
